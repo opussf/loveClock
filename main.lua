aa201b2c26fbf7f85873b29339ea84d110d1ac68
opussf <github@zz9-za.com>
2024-02-21 18:26:28 -1000

    A single clock with possible fractal iteration

--- main.lua
+++ main.lua
@@ -1,37 +1,65 @@
--- This is the code that's going to run on the our thread. It should be moved
--- to its own dedicated Lua file, but for simplicity's sake we'll create it
--- here.
-local threadCode = [[
--- Receive values sent via thread:start
-local min, max = ...
+local socket=require("socket")
 
-for i = min, max do
-    -- The Channel is used to handle communication between our main thread and
-    -- this thread. On each iteration of the loop will push a message to it which
-    -- we can then pop / receive in the main thread.
-    love.thread.getChannel( 'info' ):push( i )
+local now = {}
+local hour, minute, second = 0, 0, 0
+local width, height = 0, 0
+local centerX, centerY = 0, 0
+local fractalLoop = 1
+local handLength = {["s"] = 100, ["m"] = 75, ["h"] = 50}
+
+function valueToCoords( myCenterX, myCenterY, value, scale, hourType )
+    -- convert to rad ( based on 1-60 )
+    if hourType and hourType == 12 or hourType == 24 then
+        conversion = (2 * math.pi) / hourType
+        value = value >= hourType and value-hourType or value
+    else
+        conversion = (2 * math.pi) / 60
+    end
+    newValue = (value * conversion) - math.pi/2
+    return myCenterX,myCenterY, myCenterX+(math.cos( newValue ) * scale),myCenterY+(math.sin( newValue ) * scale)
 end
-]]
-
-local thread -- Our thread object.
-local timer = 0  -- A timer used to animate our circle.
 
 function love.load()
-    thread = love.thread.newThread( threadCode )
-    thread:start( 99, 1000 )
+    -- load function
+    width, height = love.graphics.getDimensions()
+    centerX = width/2
+    centerY = height/2
 end
 
 function love.update( dt )
-    timer = timer + dt
+    now = os.date( "*t" )
+    ms = socket.gettime()
+    second = now.sec + (ms - math.floor(ms))
+    minute = now.min + second/60
+    hour = now.hour + minute/60
 end
 
 function love.draw()
-    -- Get the info channel and pop the next message from it.
-    local info = love.thread.getChannel( 'info' ):pop()
-    if info then
-        love.graphics.print( info, timer*10, timer*10 )
+    love.graphics.print( string.format( "%i:%i.%i", hour, minute, second ), 10, 10 )
+    love.graphics.circle( 'line', centerX, centerY, handLength.s )
+    centers = {centerX, centerY}
+    newCenters = {}
+    for lcv = 0,fractalLoop-1 do
+        for i = 1,#centers,2 do 
+            cX = centers[i]
+            cY = centers[i+1]
+            --print( cX, cY, #centers )
+            for _,v in ipairs({drawClock( cX, cY, second*(i-1) )}) do
+                table.insert( newCenters, v )
+                --print(i,v,second)
+            end
+        end
+        centers = newCenters
     end
+end
 
-    -- We smoothly animate a circle to show that the thread isn't blocking our main thread.
-    love.graphics.circle( 'line', 100 + math.sin( timer ) * 20, 100 + math.cos( timer ) * 20, 20 )
+function drawClock( centerX, centerY, rotation )  -- seconds (1/60)
+    secondCoords = {valueToCoords( centerX, centerY, second+rotation, handLength.s )}
+    minuteCoords = {valueToCoords( centerX, centerY, minute+rotation, handLength.m )}
+    hourCoords = {valueToCoords( centerX, centerY, hour+(rotation/5), handLength.h, 12 )}
+
+    love.graphics.line( secondCoords )
+    love.graphics.line( minuteCoords )
+    love.graphics.line( hourCoords )
+    return secondCoords[3], secondCoords[4], minuteCoords[3], minuteCoords[4], hourCoords[3], hourCoords[4] 
 end
